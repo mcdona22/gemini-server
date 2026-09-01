6e53a301959b3d8e4b25961f96e1ad7a24b0fe17
John McDonald <mcdona22@gmail.com>
2026-09-01 20:37:36 +0100

    can read a filename in the post method

--- src/features/receipt/receipt.controller.ts
+++ src/features/receipt/receipt.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, UploadedFile, Patch, Param, Delete, UseInterceptors } from '@nestjs/common';
 import { ReceiptService } from './receipt.service.js';
 import { CreateReceiptDto } from './dto/create-receipt.dto.js';
 import { UpdateReceiptDto } from './dto/update-receipt.dto.js';
+import { FileInterceptor } from '@nestjs/platform-express';
+import 'multer';
 
 @Controller('receipt')
 export class ReceiptController {
   constructor(private readonly receiptService: ReceiptService) {}
 
   @Post()
-  create(@Body() createReceiptDto: CreateReceiptDto) {
-    return this.receiptService.create(createReceiptDto);
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file:  Express.Multer.File) {
+    return { fileName:file.originalname };
   }
 
   @Get()
