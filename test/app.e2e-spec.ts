b4d44a67d0d8224dbf2d82dae8c18cb1bfb33d2d
John McDonald <mcdona22@gmail.com>
2026-09-16 00:16:56 +0100

    refactored tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -48,6 +48,12 @@ describe('Throttling behaviour (e2e)', () => {
     describe('Receipt endpoint', () => {
       const receiptEndPoint = `/${endPoint}/receipt`;
       const analyseUrl = `/api/v1/receipt/analyse`;
+      const dummyFileBuffer = Buffer.from('mock image payload');
+
+      const postAnalyseRequest = (url: string) =>
+        request(httpServer)
+          .post(url)
+          .attach('file', dummyFileBuffer, 'receipt.jpeg');
 
       it('should respond with 200 for noddy GET', async () => {
         return request(httpServer).get(receiptEndPoint).expect(HttpStatus.OK);
@@ -61,96 +67,28 @@ describe('Throttling behaviour (e2e)', () => {
 
       it('should not throttle POST within the limit', async () => {
         const limit = 3;
-        const dummyFileBuffer = Buffer.from('mock image payload');
 
         for (let i = 0; i < limit; ++i) {
-          const response = await request(httpServer)
-            .post(analyseUrl)
-            .attach('file', dummyFileBuffer, 'receipt.jpeg');
-
+          const response = await postAnalyseRequest(analyseUrl);
           expect(response.statusCode).to.equal(HttpStatus.CREATED);
         }
       });
 
-      it('should  throttle POST in excess of the limit', async () => {
+      it('should throttle POST in excess of the limit', async () => {
         const limit = 3;
-        const dummyFileBuffer = Buffer.from('mock image payload');
 
         for (let i = 0; i < limit; ++i) {
-          const response = await request(httpServer)
-            .post(analyseUrl)
-            .attach('file', dummyFileBuffer, 'receipt.jpeg');
-
+          const response = await postAnalyseRequest(analyseUrl);
           expect(response.statusCode).to.equal(HttpStatus.CREATED);
         }
 
-        const response = await request(httpServer)
-          .post(analyseUrl)
-          .attach('file', dummyFileBuffer, 'receipt.jpeg');
-        expect(response.statusCode).to.equal(HttpStatus.TOO_MANY_REQUESTS);
+        const throttledResponse = await postAnalyseRequest(analyseUrl);
+        expect(throttledResponse.statusCode).to.equal(
+          HttpStatus.TOO_MANY_REQUESTS,
+        );
       });
     });
-
-    // it('GET should NOT throttle requests when thresholds exceeded', async () => {
-    //   const limit = 3;
-    //   for (let i = 0; i < limit; ++i) {
-    //     const res = await request(httpServer).get(rootEndPoint);
-    //     expect(res.statusCode).to.equal(HttpStatus.OK);
-    //   }
-    //   const throttledRes = await request(httpServer).get(rootEndPoint);
-    //   expect(throttledRes.status).toBe(HttpStatus.OK);
-    // });
   });
-
-  // describe('POST on root endpoint should be throttled', async () => {
-  //   const receiptEndpoint = `/${endPoint}/receipt`;
-  //   const analyseEndpoint = `/${receiptEndpoint}/analyse`;
-  //
-  //   it('should return the POST data correctly', async () => {
-  //     return (
-  //       request(httpServer)
-  //         .get('/api/v1')
-  //         .expect(HttpStatus.OK)
-  //         // .expect('Content-Type', /json/)
-  //         .expect((response) => {
-  //           expect(response.body).toHaveProperty('message', 'POST RECEIVED');
-  //         })
-  //     );
-  //   });
-  //
-  //   // it.skip('should return the POST data correctly', async () => {
-  //   //   return request(httpServer)
-  //   //     .post(receiptEndpoint)
-  //   //     .expect(HttpStatus.CREATED)
-  //   //     .expect('Content-Type', /json/)
-  //   //     .expect((response) => {
-  //   //       expect(response.body).toHaveProperty('message', 'POST RECEIVED');
-  //   //     });
-  //   // });
-  //
-  //   // it('should NOT throttle the GET endpoint', async () => {
-  //   //   const limit = 3;
-  //   //   for (let i = 0; i < limit; ++i) {
-  //   //     const res = await request(httpServer).get(receiptEndpoint);
-  //   //     expect(res.statusCode).to.equal(200);
-  //   //   }
-  //   //   const throttledRes = await request(httpServer).get(receiptEndpoint);
-  //   //   expect(throttledRes.status).toBe(HttpStatus.OK);
-  //   // });
-  //
-  //   it('should throttle the POST endpoint', async () => {
-  //     const limit = 3;
-  //     const dummyFileBuffer = Buffer.from('mock image payload');
-  //     for (let i = 0; i < limit; ++i) {
-  //       const res = await request(httpServer)
-  //         .post('/api/v2/receipt/analyse/')
-  //         .attach('file', dummyFileBuffer, 'test-receipt.jpg');
-  //       expect(res.statusCode).to.equal(HttpStatus.CREATED);
-  //     }
-  //     const throttledRes = await request(httpServer).post(receiptEndpoint);
-  //     expect(throttledRes.status).toBe(HttpStatus.TOO_MANY_REQUESTS);
-  //   });
-  // });
 
   afterEach(async () => {
     await app.close();
