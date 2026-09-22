6dd067ebc2b7de48fb9886482dc48e8ff33cedbf
John McDonald <mcdona22@gmail.com>
2026-09-22 02:49:46 +0100

    reduced cors surface area

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    methods: 'GET,POST',
     credentials: true,
     allowedHeaders: ['content-type', 'Authorization', 'Accept'],
   });
